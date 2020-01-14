8f9c2f656dc1190b6655fbb9df3f0ede837dade6
yunus <yycan77@hotmail.com>
2020-01-14 16:22:20 +0300

    Add new test

---
+++ tests/lots_of_args.v
@@ -0,0 +1,12 @@
+var var1 :int , var2 : int, var3 :int , var4:int ,var5: int[2], var6:int,var7:real;
+
+int func bar(var1 : int, var2: int, var3 :int , var4:int ,var5: int[2], var6:int,var7:real,main_arg:real, var8 : real, var9:real, var10:int[23], var11 : real)
+    return 1;
+endfunc
+
+int func main(main_arg:real)
+    var var8 : real, var9:real, var10:int[23], var11 : real;
+    var test1 : int[4];
+    bar(var1,var2,var3,var4,var5,var6,var7,main_arg,var8,var9,var10,var11);
+    return 5+4;
+endfunc
